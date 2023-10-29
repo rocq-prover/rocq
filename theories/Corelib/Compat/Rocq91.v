f1c0905c73f4f6a2f04c19d297188e8f1f70ded1
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2023-10-29 15:14:49 +0100

    Gramlib: warn when recovery mode is used.

---
+++ theories/Corelib/Compat/Rocq91.v
@@ -0,0 +1,16 @@
+(************************************************************************)
+(*         *      The Rocq Prover / The Rocq Development Team           *)
+(*  v      *         Copyright INRIA, CNRS and contributors             *)
+(* <O___,, * (see version control and CREDITS file for authors & dates) *)
+(*   \VV/  **************************************************************)
+(*    //   *    This file is distributed under the terms of the         *)
+(*         *     GNU Lesser General Public License Version 2.1          *)
+(*         *     (see LICENSE file for the text of the license)         *)
+(************************************************************************)
+
+(** Compatibility file for making Rocq act similar to Coq v9.1 *)
+
+(** When removing this file, please cleanup the "-compat" option code
+    in sysinit/coqargs.ml *)
+
+#[export] Set Warnings "-deprecated-since-9.2".
