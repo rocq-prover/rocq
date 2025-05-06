8e4d65a81d3dd490c1912c07635a0fcd25205f8e
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-05-06 15:10:23 +0200

    Move Flags.record_comments to CLexer

--- lib/flags.ml
+++ lib/flags.ml
@@ -58,7 +58,6 @@ let in_synterp_phase = ref false
 (* Translate *)
 let beautify = ref false
 let beautify_file = ref false
-let record_comments = ref false
 
 (* Silent / Verbose *)
 let quiet = ref false
