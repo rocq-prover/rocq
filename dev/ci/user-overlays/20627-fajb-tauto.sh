155905eb72b6d819eac3e20423e415902db9c477
Frédéric Besson <frederic.besson@inria.fr>
2025-05-21 10:12:01 +0200

    overlay

--- dev/ci/user-overlays/20627-fajb-tauto.sh
+++ dev/ci/user-overlays/20627-fajb-tauto.sh
@@ -1,1 +1,12 @@
 overlay stdlib https://github.com/fajb/stdlib tauto 20627
+
+overlay bedrock2 https://github.com/fajb/bedrock2 tauto 20627
+# Make PRs against https://github.com/mit-plv/bedrock2 base branch master
+
+overlay corn https://github.com/fajb/corn tauto 20627
+
+overlay vst https://github.com/fajb/VST tauto 20627
+
+overlay coq_dpdgraph https://github.com/fajb/coq-dpdgraph tauto 20627
+
+overlay metarocq https://github.com/fajb/metarocq tauto 20627
