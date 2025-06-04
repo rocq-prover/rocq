e8121646e71c5bac541208d81b5a83e33b371082
Enrico Tassi <Enrico.Tassi@Inria.fr>
2025-06-04 17:03:33 +0200

    overlay

---
+++ dev/ci/user-overlays/20707-gares-ssrpat-FO-ignore-imparg.sh
@@ -0,0 +1,9 @@
+overlay mathcomp https://github.com/gares/math-comp ssrpat-FO-ignore-imparg 20707
+
+overlay perennial https://github.com/gares/perennial ssrpat-FO-ignore-imparg 20707
+
+overlay oddorder https://github.com/gares/odd-order ssrpat-FO-ignore-imparg 20707
+
+overlay analysis https://github.com/gares/analysis ssrpat-FO-ignore-imparg 20707
+
+overlay jasmin https://github.com/gares/jasmin ssrpat-FO-ignore-imparg 20707
