cfa6f32f20cf05d2017f92a8be35cddbc92ed3d8
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-07-25 15:23:35 +0200

    Experiment to improve parsing errors
    eg on `Open Scope Type` we used to get
    `Syntax error: illegal begin of toplevel:vernac_toplevel`
    now it also says `after Open Scope, expected identifier`.
    
    The current state is not entirely satisfying, eg on `Doesntexist.` it
    gives the list of command initial keywords, with "end of input" in the
    middle, yet is not a complete list of possible beginnings such as
    attributes (as can be seen in the test suite).

--- parsing/tok.mli
+++ parsing/tok.mli
@@ -36,12 +36,14 @@ type t =
 
 val equal_p : 'a p -> 'b p -> ('a, 'b) Util.eq option
 
+val equal : t -> t -> bool
+
 (* pass true for diff_mode *)
 val extract_string : bool -> t -> string
 
 (** Names of tokens, used in Grammar error messages *)
 
-val token_text : 'c p -> string
+val pattern_text : 'c p -> string
 
 (** Utility function for the test returned by a QUOTATION token:
     It returns the delimiter parenthesis, if any, and the text
