b6973a37f12839b9e92866ef99f7ed87325a183d
Pierre Roux <pierre.roux@onera.fr>
2025-09-11 10:36:20 +0200

    Retrieve a few operations on binary natural numbers N

--- theories/Corelib/BinNums/NatDef.v
+++ theories/Corelib/BinNums/NatDef.v
@@ -10,11 +10,28 @@
 
 From Corelib Require Export BinNums PosDef.
 
-(**********************************************************************)
+(********************************************************)
 (** * Binary natural numbers, definitions of operations *)
-(**********************************************************************)
+(********************************************************)
 
 Module N.
+
+(** ** Successor *)
+
+Definition succ n :=
+  match n with
+  | N0 => Npos xH
+  | Npos p => Npos (Pos.succ p)
+  end.
+
+(** ** Addition *)
+
+Definition add n m :=
+  match n, m with
+  | N0, _ => m
+  | _, N0 => n
+  | Npos p, Npos q => Npos (Pos.add p q)
+  end.
 
 (** ** Operation [x -> 2*x+1] *)
 
@@ -53,6 +70,15 @@ match n, m with
   end
 end.
 
+(** Multiplication *)
+
+Definition mul n m :=
+  match n, m with
+  | N0, _ => N0
+  | _, N0 => N0
+  | Npos p, Npos q => Npos (Pos.mul p q)
+  end.
+
 (** Order *)
 
 Definition compare n m :=
@@ -64,6 +90,13 @@ Definition compare n m :=
   end.
 
 (** Boolean equality and comparison *)
+
+Definition eqb n m :=
+  match n, m with
+    | N0, N0 => true
+    | Npos p, Npos q => Pos.eqb p q
+    | _, _ => false
+  end.
 
 Definition leb x y :=
  match compare x y with Gt => false | _ => true end.
@@ -122,6 +155,26 @@ Definition lxor n m :=
     | N0, _ => m
     | _, N0 => n
     | Npos p, Npos q => Pos.lxor p q
+  end.
+
+(** Translation from [N] to [nat] and back. *)
+
+Definition to_nat (a : N) :=
+  match a with
+    | N0 => O
+    | Npos p => Pos.to_nat p
+  end.
+
+(** Conversion with a decimal representation for printing/parsing *)
+
+Definition of_uint (d:Decimal.uint) := Pos.of_uint d.
+
+Definition of_hex_uint (d:Hexadecimal.uint) := Pos.of_hex_uint d.
+
+Definition of_num_uint (d:Number.uint) :=
+  match d with
+  | Number.UIntDecimal d => of_uint d
+  | Number.UIntHexadecimal d => of_hex_uint d
   end.
 
 End N.
