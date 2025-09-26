d87402435ab5805e7486dbdb28f08213ef5b11ac
ia0 <git@ia0.eu>
2025-09-26 20:45:04 +0200

    Experiment: stricter associativity
    This PR is for discussion purpose only. It is not meant to be merged in its current form and should
    probably be rewritten by someone more experimented in OCaml and more familiar with Rocq
    implementation if it were to be ever merged.
    
    This is a follow-up of the discussion in #21072, initially started by #21029. This can also be seen
    as the continuation of the work started in #17876. In particular, this PR provides proper support
    for non-associativity and fixes leniency in right-associativity (namely that the left component
    recognizes the non-recursive part of the same level instead of the next level only).
    
    The file `foo.v` kind of gives an overview of the change. Commented lines are rejected by parsing
    (they emit the recovery warning). Commented lines commented with `(* FIXED *)` are only rejected by
    this PR (they are accepted by `master`). Uncommented lines are parsed as expected.
    
    This was only tested with notations, which don't permit to express every possible rule (in
    particular SELF on the right for right-associativity). It would be good to test those too.
    
    This PR also has a known issue regarding level and span in "warn recover" messages. I'm not sure
    where this comes from. I suspect we don't recover from the same place and possibly don't even call
    the same "warn recover" function.

--- gramlib/grammar.ml
+++ gramlib/grammar.ml
@@ -145,7 +145,7 @@ module type ExtS = sig
       estate : EState.t;
       kwstate : keyword_state;
       recover : bool;
-      has_non_assoc : bool;
+      has_non_assoc : bool; (* not used anymore *)
     }
   end
 
@@ -1421,9 +1421,16 @@ let rec start_parser_of_levels entry clevn =
                   let bp = LStream.count strm__ in
                   match p2 gstate strm__ with
                   | Ok act ->
+                      (* this can probably be factorized with the same code below *)
                       let ep = LStream.count strm__ in
                       let a = act (LStream.interval_loc bp ep strm__) in
-                      continue_parser_of_entry gstate entry (Some clevn) levn bp a strm
+                      if lev.assoc <> LeftA then
+                        if clevn = levn then
+                          Ok a
+                        else
+                          continue_parser_of_entry gstate entry (Some (clevn-1)) levn bp a strm
+                      else
+                        continue_parser_of_entry gstate entry (Some clevn) levn bp a strm
                   | Error () -> p1 gstate levn strm__
 
 (** [continue_parser_of_levels entry clevn levels levn bp a strm] goes
@@ -1465,7 +1472,7 @@ let rec continue_parser_of_levels entry clevn =
                   let* act = p2 gstate strm__ in
                   let ep = LStream.count strm__ in
                   let a = act a (LStream.interval_loc bp ep strm__) in
-                  if gstate.has_non_assoc && lev.assoc = NonA then
+                  if lev.assoc <> LeftA then
                     if clevn = levn then
                       Ok a
                     else
