jw960/coq

[scoqc] Support `Reset Initial`

compiler/scoqc.ml
@@ -38,6 +38,13 @@ let time_output ~time stm tstart tend =
     Feedback.msg_notice pp
   | Some (ToFile _file) -> ()
 
+let init_st = ref None
+
+let execute ~st (cmd : Vernacexpr.vernac_control) =
+  match cmd.v.expr with
+  | Vernacexpr.VernacSynPure VernacResetInitial -> Option.get !init_st
+  | _ -> execute ~st cmd
+
 let rec cloop ~time ~st pa =
   match parse ~st pa with
   | None ->
@@ -94,7 +101,7 @@ let rec process_extra args = match args with
     file, r :: extra
   | [] -> None,[]
 
-let compile ~args ~injections ~extra =
+let compile_files ~args ~injections ~extra =
   let out_file, extra = process_extra extra in
   let in_file = match extra with
     | [] -> exit 0
@@ -105,6 +112,7 @@ let compile ~args ~injections ~extra =
   let libname = Util.dirpath_of_file in_file in
   let () = Coqinit.start_library ~top:libname injections in
   let st = Vernacstate.freeze_full_state ~marshallable:false in
+  init_st := Some st;
   let loc = Loc.(initial (InFile { dirpath = None; file = in_file })) in
   let pa = Pcoq.Parsable.make ~loc (Gramlib.Stream.of_channel f_in) in
   let time = args.Coqargs.time in
@@ -128,7 +136,7 @@ let () =
     let args, extra = Coqinit.parse_arguments ~parse_extra ~usage () in
     ignore (Feedback.add_feeder Util.fb_handler);
     let injections = Coqinit.init_runtime args in
-    compile ~args:(args.config) ~injections ~extra
+    compile_files ~args:(args.config) ~injections ~extra
   with exn ->
     let exn, info = Exninfo.capture exn in
     let loc = Option.append (Loc.get_loc info) !error_loc in

rocq-prover/rocq

compiler/scoqc.ml
@@ -41,6 +41,13 @@ let time_output ~time stm tstart tend =
     Feedback.msg_notice pp
   | Some (ToFile _file) -> ()
 
+let init_st = ref None
+
+let execute ~st (cmd : Vernacexpr.vernac_control) =
+  match cmd.v.expr with
+  | Vernacexpr.VernacSynPure VernacResetInitial -> Option.get !init_st
+  | _ -> execute ~st cmd
+
 let rec cloop ~time ~st pa =
   match parse ~st pa with
   | None ->
@@ -97,7 +104,7 @@ let rec process_extra args = match args with
     file, r :: extra
   | [] -> None,[]
 
-let compile ~args ~injections ~extra =
+let compile_files ~args ~injections ~extra =
   let out_file, extra = process_extra extra in
   let in_file = match extra with
     | [] -> exit 0
@@ -137,7 +144,7 @@ let main args =
     (* TODO *)
     (* let injections = Coqargs.injection_commands opts) in *)
     let injections = [] in
-    compile ~args:(opts.config) ~injections ~extra
+    compile_files ~args:(opts.config) ~injections ~extra
   with exn ->
     let exn, info = Exninfo.capture exn in
     let loc = Option.append (Loc.get_loc info) !error_loc in