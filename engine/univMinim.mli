TDiazT/coq

Add support for parsing elimination constraints

engine/univMinim.mli
@@ -9,6 +9,7 @@
 (************************************************************************)
 
 open Univ
+open PolyConstraints
 
 (** Unordered pairs of universe levels (ie (u,v) = (v,u)) *)
 module UPairSet : CSet.S with type elt = (Level.t * Level.t)
@@ -36,4 +37,4 @@ val extra_union : extra -> extra -> extra
 val normalize_context_set : UGraph.t -> ContextSet.t ->
   UnivFlex.t (* The defined and undefined variables *) ->
   extra ->
-  UnivFlex.t in_universe_context_set
+  UnivFlex.t in_poly_context_set

interp/modintern.ml
@@ -25,7 +25,7 @@ exception ModuleInternalizationError of module_internalization_error
 
 type module_kind = Module | ModType | ModAny
 
-type module_struct_expr = (universe_decl_expr option * constr_expr) Declarations.module_alg_expr
+type module_struct_expr = (poly_decl_expr option * constr_expr) Declarations.module_alg_expr
 
 let error_not_a_module_loc ~info kind loc qid =
   let e = match kind with
@@ -131,17 +131,17 @@ let rec intern_module_ast kind m = match m with
       (MEwith(me,decl), base, kind)
 
 let interp_with_decl env base kind = function
-  | WithMod (fqid,mp) -> WithMod (fqid,mp), Univ.ContextSet.empty
+  | WithMod (fqid,mp) -> WithMod (fqid,mp), PolyConstraints.ContextSet.empty
   | WithDef(fqid,(udecl,c)) ->
-    let sigma, udecl = interp_univ_decl_opt env udecl in
+    let sigma, udecl = interp_poly_decl_opt env udecl in
     let c, ectx = interp_constr env sigma c in
     let poly = lookup_polymorphism env base kind fqid in
-    begin match fst (UState.check_univ_decl ~poly ectx udecl) with
+    begin match fst (UState.check_poly_decl ~poly ectx udecl) with
       | UState.Polymorphic_entry ctx ->
         let inst, ctx = UVars.abstract_universes ctx in
         let c = EConstr.Vars.subst_univs_level_constr (UVars.make_instance_subst inst) c in
         let c = EConstr.to_constr sigma c in
-        WithDef (fqid,(c, Some ctx)), Univ.ContextSet.empty
+        WithDef (fqid,(c, Some ctx)), PolyConstraints.ContextSet.empty
       | UState.Monomorphic_entry ctx ->
         let c = EConstr.to_constr sigma c in
         WithDef (fqid,(c, None)), ctx
@@ -156,8 +156,8 @@ let rec interp_module_ast env kind base m cst = match m with
 | MEwith(me,decl) ->
   let me, cst = interp_module_ast env kind base me cst in
   let decl, cst' = interp_with_decl env base kind decl in
-  let cst = Univ.ContextSet.union cst cst' in
+  let cst = PolyConstraints.ContextSet.union cst cst' in
   MEwith(me,decl), cst
 
 let interp_module_ast env kind base m =
-  interp_module_ast env kind base m Univ.ContextSet.empty
+  interp_module_ast env kind base m PolyConstraints.ContextSet.empty

kernel/constant_typing.ml
@@ -20,7 +20,7 @@ open Constr
 open Declarations
 open Environ
 open Entries
-open Univ
+open PolyConstraints
 open UVars
 
 module NamedDecl = Context.Named.Declaration
@@ -103,7 +103,7 @@ let process_universes env = function
   | Entries.Polymorphic_entry uctx ->
     (** [ctx] must contain local universes, such that it has no impact
         on the rest of the graph (up to transitivity). *)
-    let env = Environ.push_context ~strict:false uctx env in
+    let env = Environ.push_context ~strict:false QGraph.Rigid uctx env in
     let inst, auctx = UVars.abstract_universes uctx in
     let usubst = UVars.make_instance_subst inst in
     env, usubst, inst, Polymorphic auctx
@@ -125,7 +125,7 @@ let adjust_primitive_univ_entry p auctx = function
        the instance so comparing the sizes works. No polymorphic
        primitive uses constraints currently. *)
     if not (AbstractContext.size auctx = UContext.size uctx
-            && UnivConstraints.is_empty (UContext.constraints uctx))
+            && PolyConstraints.is_empty (UContext.constraints uctx))
     then CErrors.user_err Pp.(str "Incorrect universes for primitive " ++
                                 str (CPrimitives.op_or_type_to_string p));
     Polymorphic_entry (UContext.refine_names (AbstractContext.names auctx) uctx)
@@ -267,11 +267,11 @@ let check_delayed (type a) (handle : a effect_handler) tyenv (body : a proof_out
   let env, univs = match univs with
     | Monomorphic ->
        assert (UVars.is_empty_sort_subst usubst);
-       push_context_set uctx env, Opaqueproof.PrivateMonomorphic uctx
+       push_context_set QGraph.Static uctx env, Opaqueproof.PrivateMonomorphic uctx
     | Polymorphic _ ->
        assert (Int.equal valid_signatures 0);
        push_subgraph uctx env,
-       let private_univs = on_snd (subst_univs_level_constraints (snd usubst)) uctx in
+       let private_univs = on_snd (subst_univs_constraints usubst) uctx in
        Opaqueproof.PrivatePolymorphic private_univs
   in
   (* Note: non-trivial trusted side-effects only in monomorphic case *)

proofs/clenv.ml
@@ -67,7 +67,7 @@ let merge_fsorts evd clenv =
   in
   let fsorts = List.fold_left fold Univ.Level.Set.empty clenv.metas in
   let fsorts = Univ.Level.Set.filter filter fsorts in
-  let uctx = (fsorts, Univ.UnivConstraints.empty) in
+  let uctx = (fsorts, PolyConstraints.empty) in
   Evd.merge_context_set Evd.univ_flexible evd uctx
 
 let update_clenv_evd clenv evd metam =
@@ -126,21 +126,21 @@ let refresh_template_constraints ~metas env sigma ind c =
   let mib = Environ.lookup_mind (fst ind) env in
   let ctx = (Option.get mib.mind_template).template_context in
   let cstrs0 = UVars.UContext.constraints @@ UVars.AbstractContext.repr ctx in
-  if Univ.UnivConstraints.is_empty cstrs0 then sigma
+  if PolyConstraints.is_empty cstrs0 then sigma
   else
     let _, allargs = decompose_app sigma c in
     let map c = { uj_val = c; uj_type = get_type_of_with_metas ~metas env sigma c } in
     let allargs = Array.map map allargs in
     let sigma, univs = Typing.get_template_parameters env sigma ind allargs in
     let cstrs, _, _ = Inductive.instantiate_template_universes mib univs in
-    Evd.add_constraints sigma cstrs
+    Evd.add_univ_constraints sigma (PolyConstraints.univs cstrs)
 
 let clenv_refresh env sigma ctx clenv =
   match ctx with
   | Some ctx ->
     let (subst, ctx) = UnivGen.fresh_sort_context_instance ctx in
     let emap c = Vars.subst_univs_level_constr subst c in
-    let sigma = Evd.merge_sort_context_set Evd.univ_flexible sigma ctx in
+    let sigma = Evd.merge_sort_context_set Evd.univ_flexible QGraph.Internal sigma ctx in
     (* Only metas are mentioning the old universes. *)
     mk_clausenv env sigma (Unification.Meta.map_metas emap clenv.metam) clenv.metas
       (emap clenv.templval)

vernac/comInductive.mli
@@ -55,7 +55,7 @@ type t = {
   nuparams : int option;
   univ_binders : UState.named_universes_entry;
   implicits : DeclareInd.one_inductive_impls list;
-  uctx : Univ.ContextSet.t;
+  uctx : PolyConstraints.ContextSet.t;
   where_notations : Metasyntax.notation_interpretation_decl list;
   coercions : Libnames.qualid list;
   indlocs : DeclareInd.indlocs;
@@ -81,7 +81,7 @@ type syntax_allows_template_poly = SyntaxAllowsTemplatePoly | SyntaxNoTemplatePo
 val interp_mutual_inductive_constr
   :  sigma:Evd.evar_map
   -> flags:flags
-  -> udecl:UState.universe_decl
+  -> udecl:UState.poly_decl
   -> variances:Entries.variance_entry
   -> ctx_params:EConstr.rel_context
   -> indnames:Names.Id.t list
@@ -98,7 +98,7 @@ val interp_mutual_inductive_constr
      * (* for global universe names, used by DeclareInd *)
      UState.named_universes_entry
      * (* global universes to declare before the inductive (ie without the template univs) *)
-     Univ.ContextSet.t
+     PolyConstraints.ContextSet.t
 
 (************************************************************************)
 (** Internal API, exported for Record                                   *)

vernac/declareUniv.mli
@@ -25,11 +25,11 @@ val name_mono_section_univs : Univ.Level.Set.t -> unit
 val do_universe : poly:bool -> lident list -> unit
 
 (** Command [Constraint]. *)
-val do_constraint : poly:bool -> Constrexpr.univ_constraint_expr list -> unit
+val do_constraint : poly:bool -> Constrexpr.poly_constraint_expr list -> unit
 
-val add_constraint_source : GlobRef.t -> Univ.ContextSet.t -> unit
+val add_constraint_source : GlobRef.t -> PolyConstraints.ContextSet.t -> unit
 
-val constraint_sources : unit -> (GlobRef.t * Univ.UnivConstraints.t) list
+val constraint_sources : unit -> (GlobRef.t * PolyConstraints.t) list
 (** Returns constraints associated to globrefs, newest first. *)
 
 (** Command [Sort]. *)

rocq-prover/rocq

engine/univMinim.mli
@@ -9,6 +9,7 @@
 (************************************************************************)
 
 open Univ
+open PConstraints
 
 (** Unordered pairs of universe levels (ie (u,v) = (v,u)) *)
 module UPairSet : CSet.S with type elt = (Level.t * Level.t)
@@ -36,4 +37,4 @@ val extra_union : extra -> extra -> extra
 val normalize_context_set : UGraph.t -> ContextSet.t ->
   UnivFlex.t (* The defined and undefined variables *) ->
   extra ->
-  UnivFlex.t in_universe_context_set
+  UnivFlex.t in_poly_context_set

interp/modintern.ml
@@ -25,7 +25,7 @@ exception ModuleInternalizationError of module_internalization_error
 
 type module_kind = Module | ModType | ModAny
 
-type module_struct_expr = (universe_decl_expr option * constr_expr) Declarations.module_alg_expr
+type module_struct_expr = (sort_poly_decl_expr option * constr_expr) Declarations.module_alg_expr
 
 let error_not_a_module_loc ~info kind loc qid =
   let e = match kind with
@@ -131,17 +131,17 @@ let rec intern_module_ast kind m = match m with
       (MEwith(me,decl), base, kind)
 
 let interp_with_decl env base kind = function
-  | WithMod (fqid,mp) -> WithMod (fqid,mp), Univ.ContextSet.empty
+  | WithMod (fqid,mp) -> WithMod (fqid,mp), PConstraints.ContextSet.empty
   | WithDef(fqid,(udecl,c)) ->
-    let sigma, udecl = interp_univ_decl_opt env udecl in
+    let sigma, udecl = interp_sort_poly_decl_opt env udecl in
     let c, ectx = interp_constr env sigma c in
     let poly = lookup_polymorphism env base kind fqid in
-    begin match fst (UState.check_univ_decl ~poly ectx udecl) with
+    begin match fst (UState.check_sort_poly_decl ~poly ectx udecl) with
       | UState.Polymorphic_entry ctx ->
         let inst, ctx = UVars.abstract_universes ctx in
         let c = EConstr.Vars.subst_univs_level_constr (UVars.make_instance_subst inst) c in
         let c = EConstr.to_constr sigma c in
-        WithDef (fqid,(c, Some ctx)), Univ.ContextSet.empty
+        WithDef (fqid,(c, Some ctx)), PConstraints.ContextSet.empty
       | UState.Monomorphic_entry ctx ->
         let c = EConstr.to_constr sigma c in
         WithDef (fqid,(c, None)), ctx
@@ -156,8 +156,8 @@ let rec interp_module_ast env kind base m cst = match m with
 | MEwith(me,decl) ->
   let me, cst = interp_module_ast env kind base me cst in
   let decl, cst' = interp_with_decl env base kind decl in
-  let cst = Univ.ContextSet.union cst cst' in
+  let cst = PConstraints.ContextSet.union cst cst' in
   MEwith(me,decl), cst
 
 let interp_module_ast env kind base m =
-  interp_module_ast env kind base m Univ.ContextSet.empty
+  interp_module_ast env kind base m PConstraints.ContextSet.empty

kernel/constant_typing.ml
@@ -20,7 +20,7 @@ open Constr
 open Declarations
 open Environ
 open Entries
-open Univ
+open PConstraints
 open UVars
 
 module NamedDecl = Context.Named.Declaration
@@ -103,7 +103,7 @@ let process_universes env = function
   | Entries.Polymorphic_entry uctx ->
     (** [ctx] must contain local universes, such that it has no impact
         on the rest of the graph (up to transitivity). *)
-    let env = Environ.push_context ~strict:false uctx env in
+    let env = Environ.push_context ~strict:false QGraph.Rigid uctx env in
     let inst, auctx = UVars.abstract_universes uctx in
     let usubst = UVars.make_instance_subst inst in
     env, usubst, inst, Polymorphic auctx
@@ -125,7 +125,7 @@ let adjust_primitive_univ_entry p auctx = function
        the instance so comparing the sizes works. No polymorphic
        primitive uses constraints currently. *)
     if not (AbstractContext.size auctx = UContext.size uctx
-            && UnivConstraints.is_empty (UContext.constraints uctx))
+            && PConstraints.is_empty (UContext.constraints uctx))
     then CErrors.user_err Pp.(str "Incorrect universes for primitive " ++
                                 str (CPrimitives.op_or_type_to_string p));
     Polymorphic_entry (UContext.refine_names (AbstractContext.names auctx) uctx)
@@ -267,11 +267,11 @@ let check_delayed (type a) (handle : a effect_handler) tyenv (body : a proof_out
   let env, univs = match univs with
     | Monomorphic ->
        assert (UVars.is_empty_sort_subst usubst);
-       push_context_set uctx env, Opaqueproof.PrivateMonomorphic uctx
+       push_context_set QGraph.Static uctx env, Opaqueproof.PrivateMonomorphic uctx
     | Polymorphic _ ->
        assert (Int.equal valid_signatures 0);
        push_subgraph uctx env,
-       let private_univs = on_snd (subst_univs_level_constraints (snd usubst)) uctx in
+       let private_univs = on_snd (subst_univs_constraints usubst) uctx in
        Opaqueproof.PrivatePolymorphic private_univs
   in
   (* Note: non-trivial trusted side-effects only in monomorphic case *)

proofs/clenv.ml
@@ -67,7 +67,7 @@ let merge_fsorts evd clenv =
   in
   let fsorts = List.fold_left fold Univ.Level.Set.empty clenv.metas in
   let fsorts = Univ.Level.Set.filter filter fsorts in
-  let uctx = (fsorts, Univ.UnivConstraints.empty) in
+  let uctx = (fsorts, PConstraints.empty) in
   Evd.merge_context_set Evd.univ_flexible evd uctx
 
 let update_clenv_evd clenv evd metam =
@@ -126,21 +126,21 @@ let refresh_template_constraints ~metas env sigma ind c =
   let mib = Environ.lookup_mind (fst ind) env in
   let ctx = (Option.get mib.mind_template).template_context in
   let cstrs0 = UVars.UContext.constraints @@ UVars.AbstractContext.repr ctx in
-  if Univ.UnivConstraints.is_empty cstrs0 then sigma
+  if PConstraints.is_empty cstrs0 then sigma
   else
     let _, allargs = decompose_app sigma c in
     let map c = { uj_val = c; uj_type = get_type_of_with_metas ~metas env sigma c } in
     let allargs = Array.map map allargs in
     let sigma, univs = Typing.get_template_parameters env sigma ind allargs in
     let cstrs, _, _ = Inductive.instantiate_template_universes mib univs in
-    Evd.add_constraints sigma cstrs
+    Evd.add_univ_constraints sigma (PConstraints.univs cstrs)
 
 let clenv_refresh env sigma ctx clenv =
   match ctx with
   | Some ctx ->
     let (subst, ctx) = UnivGen.fresh_sort_context_instance ctx in
     let emap c = Vars.subst_univs_level_constr subst c in
-    let sigma = Evd.merge_sort_context_set Evd.univ_flexible sigma ctx in
+    let sigma = Evd.merge_sort_context_set Evd.univ_flexible QGraph.Internal sigma ctx in
     (* Only metas are mentioning the old universes. *)
     mk_clausenv env sigma (Unification.Meta.map_metas emap clenv.metam) clenv.metas
       (emap clenv.templval)

vernac/comInductive.mli
@@ -55,7 +55,7 @@ type t = {
   nuparams : int option;
   univ_binders : UState.named_universes_entry;
   implicits : DeclareInd.one_inductive_impls list;
-  uctx : Univ.ContextSet.t;
+  uctx : PConstraints.ContextSet.t;
   where_notations : Metasyntax.notation_interpretation_decl list;
   coercions : Libnames.qualid list;
   indlocs : DeclareInd.indlocs;
@@ -81,7 +81,7 @@ type syntax_allows_template_poly = SyntaxAllowsTemplatePoly | SyntaxNoTemplatePo
 val interp_mutual_inductive_constr
   :  sigma:Evd.evar_map
   -> flags:flags
-  -> udecl:UState.universe_decl
+  -> udecl:UState.sort_poly_decl
   -> variances:Entries.variance_entry
   -> ctx_params:EConstr.rel_context
   -> indnames:Names.Id.t list
@@ -98,7 +98,7 @@ val interp_mutual_inductive_constr
      * (* for global universe names, used by DeclareInd *)
      UState.named_universes_entry
      * (* global universes to declare before the inductive (ie without the template univs) *)
-     Univ.ContextSet.t
+     PConstraints.ContextSet.t
 
 (************************************************************************)
 (** Internal API, exported for Record                                   *)

vernac/declareUniv.mli
@@ -25,11 +25,11 @@ val name_mono_section_univs : Univ.Level.Set.t -> unit
 val do_universe : poly:bool -> lident list -> unit
 
 (** Command [Constraint]. *)
-val do_constraint : poly:bool -> Constrexpr.univ_constraint_expr list -> unit
+val do_constraint : poly:bool -> Constrexpr.sort_poly_constraint_expr list -> unit
 
-val add_constraint_source : GlobRef.t -> Univ.ContextSet.t -> unit
+val add_constraint_source : GlobRef.t -> PConstraints.ContextSet.t -> unit
 
-val constraint_sources : unit -> (GlobRef.t * Univ.UnivConstraints.t) list
+val constraint_sources : unit -> (GlobRef.t * PConstraints.t) list
 (** Returns constraints associated to globrefs, newest first. *)
 
 (** Command [Sort]. *)