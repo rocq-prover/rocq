coq/coq

[core] Improved state protection against memprof-limits interruptions
This is a proof-of-concept (based on #19175), but could be merged as
is.

It should fix a few bugs we had when interrupting Coq:

- `Pcoq.unfreeze` caused grammar state to go wrong (in a non
  recoverable way)
- Interrupting module dynlink was also leaving the system in a bad
  state.

It is hard to review the codebase for potential problems like this.

lib/control.ml
@@ -47,19 +47,11 @@ let unix_timeout n f x =
       let _ = setitimer ITIMER_REAL { old_timer with it_value = old_timer_value } in
       Sys.set_signal Sys.sigalrm psh
     in
-    try
-      let _ = setitimer ITIMER_REAL {it_interval = 0.; it_value = n} in
-      let res = f x in
-      restore_timeout ();
-      Some res
-    with
-    | Timeout ->
-      restore_timeout ();
-      None
-    | e ->
-      let e = Exninfo.capture e in
-      restore_timeout ();
-      Exninfo.iraise e
+    let open Memprof_coq.Resource_bind in
+    let& () = Memprof_coq.Masking.with_resource ~acquire:(fun () -> ()) () ~release:restore_timeout in
+    let _ = setitimer ITIMER_REAL {it_interval = 0.; it_value = n} in
+    try Some (f x)
+    with Timeout -> None
 
 let windows_timeout n f x =
   let killed = ref false in

rocq-prover/rocq

lib/control.ml
@@ -47,20 +47,13 @@ let unix_timeout n f x =
       let _ = setitimer ITIMER_REAL { old_timer with it_value = old_timer_value } in
       Sys.set_signal Sys.sigalrm psh
     in
-    try
-      let _ = setitimer ITIMER_REAL {it_interval = 0.; it_value = n} in
-      let res = f x in
-      restore_timeout ();
-      Ok res
-    with
-    | Timeout ->
-      let _, info = Exninfo.capture Timeout in
-      restore_timeout ();
+    let open Memprof_coq.Resource_bind in
+    let& () = Memprof_coq.Masking.with_resource ~acquire:(fun () -> ()) () ~release:restore_timeout in
+    let _ = setitimer ITIMER_REAL {it_interval = 0.; it_value = n} in
+    try Ok (f x)
+    with Timeout as exn ->
+      let _, info = Exninfo.capture exn in
       Error info
-    | e ->
-      let e = Exninfo.capture e in
-      restore_timeout ();
-      Exninfo.iraise e
 
 let windows_timeout n f x =
   let killed = ref false in