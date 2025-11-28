TDiazT/coq

Add support for parsing elimination constraints

kernel/type_errors.mli
@@ -75,6 +75,8 @@ type ('constr, 'types, 'r) ptype_error =
   | UnsatisfiedElimConstraints of Quality.ElimConstraints.t
   | UnsatisfiedQCumulConstraints of Quality.QCumulConstraints.t
   | UnsatisfiedUnivConstraints of UnivConstraints.t
+  | UnsatisfiedQUConstraints of Sorts.QUConstraints.t
+  | UnsatisfiedPolyConstraints of PolyConstraints.t
   | UndeclaredQualities of Quality.QVar.Set.t
   | UndeclaredUniverses of Level.Set.t
   | DisallowedSProp
@@ -152,9 +154,13 @@ val error_ill_typed_rec_body  :
 
 val error_unsatisfied_elim_constraints : env -> Quality.ElimConstraints.t -> 'a
 
-val error_unsatisfied_constraints : env -> UnivConstraints.t -> 'a
-
 val error_unsatisfied_qcumul_constraints : env -> Quality.QCumulConstraints.t -> 'a
+
+val error_unsatisfied_univ_constraints : env -> Univ.UnivConstraints.t -> 'a
+
+val error_unsatisfied_quconstraints : env -> Sorts.QUConstraints.t -> 'a
+
+val error_unsatisfied_poly_constraints : env -> PolyConstraints.t -> 'a
 
 val error_undeclared_qualities : env -> Quality.QVar.Set.t -> 'a
 

vernac/comInductive.mli
@@ -55,7 +55,7 @@ type t = {
   nuparams : int option;
   univ_binders : UState.named_universes_entry;
   implicits : DeclareInd.one_inductive_impls list;
-  uctx : Univ.ContextSet.t;
+  uctx : PolyConstraints.ContextSet.t;
   where_notations : Metasyntax.notation_interpretation_decl list;
   coercions : Libnames.qualid list;
   indlocs : DeclareInd.indlocs;
@@ -81,7 +81,7 @@ type syntax_allows_template_poly = SyntaxAllowsTemplatePoly | SyntaxNoTemplatePo
 val interp_mutual_inductive_constr
   :  sigma:Evd.evar_map
   -> flags:flags
-  -> udecl:UState.universe_decl
+  -> udecl:UState.poly_decl
   -> variances:Entries.variance_entry
   -> ctx_params:EConstr.rel_context
   -> indnames:Names.Id.t list
@@ -98,7 +98,7 @@ val interp_mutual_inductive_constr
      * (* for global universe names, used by DeclareInd *)
      UState.named_universes_entry
      * (* global universes to declare before the inductive (ie without the template univs) *)
-     Univ.ContextSet.t
+     PolyConstraints.ContextSet.t
 
 (************************************************************************)
 (** Internal API, exported for Record                                   *)

vernac/declare.mli
@@ -104,7 +104,7 @@ module Info : sig
     -> ?inline : bool
     -> ?kind : Decls.logical_kind
     (** Theorem, etc... *)
-    -> ?udecl : UState.universe_decl
+    -> ?udecl : UState.poly_decl
     -> ?scope : Locality.definition_scope
     (** locality  *)
     -> ?clearbody:bool
@@ -436,7 +436,7 @@ type constant_entry =
 
 val prepare_parameter
   : poly:bool
-  -> udecl:UState.universe_decl
+  -> udecl:UState.poly_decl
   -> types:EConstr.types
   -> Evd.evar_map
   -> Evd.evar_map * parameter_entry
@@ -467,7 +467,7 @@ val declare_definition_full
   -> body:EConstr.t
   -> ?using:Vernacexpr.section_subset_expr
   -> Evd.evar_map
-  -> GlobRef.t * Univ.ContextSet.t
+  -> GlobRef.t * PolyConstraints.ContextSet.t
 
 (** Declaration messages, for internal use *)
 

vernac/vernacentries.ml
@@ -171,7 +171,7 @@ let show_universes ~proof =
   let ctx = Evd.universe_context_set (Evd.minimize_universes sigma) in
   UState.pr (Evd.ustate sigma) ++ fnl () ++
   v 1 (str "Normalized constraints:" ++ cut() ++
-       Univ.ContextSet.pr (Termops.pr_evd_level sigma) ctx)
+       PolyConstraints.ContextSet.pr (Termops.pr_evd_qvar sigma) (Termops.pr_evd_level sigma) ctx)
 
 (* Simulate the Intro(s) tactic *)
 let show_intro ~proof all =
@@ -554,13 +554,14 @@ let mk_sources () =
   let edges =
     let libs = Library.loaded_libraries () in
     List.fold_left (fun edges dp ->
-        let _, (_, csts) = Safe_typing.univs_of_library @@ Library.library_compiled dp in
+        let _, (_,(_,univ_csts)) =
+          Safe_typing.univs_of_library @@ Library.library_compiled dp in
         UnivConstraints.fold (fun cst edges -> add_edge cst (Library dp) edges)
-          csts edges)
+          univ_csts edges)
       edges libs
   in
   let edges =
-    List.fold_left (fun edges (ref,csts) ->
+    List.fold_left (fun edges (ref,(_,csts)) ->
         UnivConstraints.fold (fun cst edges -> add_edge cst (GlobRef ref) edges)
           csts edges)
       edges srcs
@@ -1416,7 +1417,7 @@ let vernac_sort ~poly l =
 let vernac_constraint ~poly l =
   if poly && not (Lib.sections_are_opened ()) then
     user_err
-                 (str"Polymorphic universe constraints can only be declared"
+                 (str"Polymorphic constraints can only be declared"
                   ++ str " inside sections, use Monomorphic Constraint instead.");
   DeclareUniv.do_constraint ~poly l
 
@@ -2190,7 +2191,7 @@ let check_may_eval env sigma redexp rc =
     else
       let env = Evarutil.nf_env_evar sigma env in
       let env = Environ.push_quality_set qs env in
-      let env = Environ.push_context_set (us,csts) env in
+      let env = Environ.push_context_set QGraph.Static (us,csts) env in
       let c = EConstr.to_constr sigma c in
       (* OK to call kernel which does not support evars *)
       Environ.on_judgment EConstr.of_constr (Arguments_renaming.rename_typing env c)
@@ -2233,7 +2234,7 @@ let vernac_global_check c =
   let sigma = Evd.collapse_sort_variables sigma in
   let senv = Global.safe_env() in
   let uctx = Evd.universe_context_set sigma in
-  let senv = Safe_typing.push_context_set ~strict:false uctx senv in
+  let senv = Safe_typing.push_context_set ~strict:false QGraph.Static uctx senv in
   let c = EConstr.to_constr sigma c in
   let j = Safe_typing.typing senv c in
   Prettyp.print_safe_judgment j ++

rocq-prover/rocq

kernel/type_errors.mli
@@ -76,6 +76,8 @@ type ('constr, 'types, 'r) ptype_error =
   | UnsatisfiedElimConstraints of Sorts.ElimConstraints.t
   | UnsatisfiedQCumulConstraints of Sorts.QCumulConstraints.t
   | UnsatisfiedUnivConstraints of UnivConstraints.t
+  | UnsatisfiedQUConstraints of Sorts.QUConstraints.t
+  | UnsatisfiedPConstraints of PConstraints.t
   | UndeclaredQualities of Sorts.QVar.Set.t
   | UndeclaredUniverses of Level.Set.t
   | DisallowedSProp
@@ -155,9 +157,13 @@ val error_ill_typed_rec_body  :
 
 val error_unsatisfied_elim_constraints : env -> Sorts.ElimConstraints.t -> 'a
 
-val error_unsatisfied_constraints : env -> UnivConstraints.t -> 'a
-
 val error_unsatisfied_qcumul_constraints : env -> Sorts.QCumulConstraints.t -> 'a
+
+val error_unsatisfied_univ_constraints : env -> Univ.UnivConstraints.t -> 'a
+
+val error_unsatisfied_quconstraints : env -> Sorts.QUConstraints.t -> 'a
+
+val error_unsatisfied_poly_constraints : env -> PConstraints.t -> 'a
 
 val error_undeclared_qualities : env -> Sorts.QVar.Set.t -> 'a
 

vernac/comInductive.mli
@@ -56,7 +56,7 @@ type t = {
   nuparams : int option;
   univ_binders : UState.named_universes_entry;
   implicits : DeclareInd.one_inductive_impls list;
-  uctx : Univ.ContextSet.t;
+  uctx : PConstraints.ContextSet.t;
   where_notations : Metasyntax.notation_interpretation_decl list;
   coercions : Libnames.qualid list;
   indlocs : DeclareInd.indlocs;
@@ -82,7 +82,7 @@ type syntax_allows_template_poly = SyntaxAllowsTemplatePoly | SyntaxNoTemplatePo
 val interp_mutual_inductive_constr
   :  sigma:Evd.evar_map
   -> flags:flags
-  -> udecl:UState.universe_decl
+  -> udecl:UState.sort_poly_decl
   -> variances:Entries.variance_entry
   -> ctx_params:EConstr.rel_context
   -> indnames:Names.Id.t list
@@ -99,7 +99,7 @@ val interp_mutual_inductive_constr
      * (* for global universe names, used by DeclareInd *)
      UState.named_universes_entry
      * (* global universes to declare before the inductive (ie without the template univs) *)
-     Univ.ContextSet.t
+     PConstraints.ContextSet.t
 
 (************************************************************************)
 (** Internal API, exported for Record                                   *)

vernac/declare.mli
@@ -104,7 +104,7 @@ module Info : sig
     -> ?inline : bool
     -> ?kind : Decls.logical_kind
     (** Theorem, etc... *)
-    -> ?udecl : UState.universe_decl
+    -> ?udecl : UState.sort_poly_decl
     -> ?scope : Locality.definition_scope
     (** locality  *)
     -> ?clearbody:bool
@@ -436,7 +436,7 @@ type constant_entry =
 
 val prepare_parameter
   : poly:bool
-  -> udecl:UState.universe_decl
+  -> udecl:UState.sort_poly_decl
   -> types:EConstr.types
   -> Evd.evar_map
   -> Evd.evar_map * parameter_entry
@@ -467,7 +467,7 @@ val declare_definition_full
   -> body:EConstr.t
   -> ?using:Vernacexpr.section_subset_expr
   -> Evd.evar_map
-  -> GlobRef.t * Univ.ContextSet.t
+  -> GlobRef.t * PConstraints.ContextSet.t
 
 (** Declaration messages, for internal use *)
 

vernac/vernacentries.ml
@@ -171,7 +171,7 @@ let show_universes ~proof =
   let ctx = Evd.universe_context_set (Evd.minimize_universes sigma) in
   UState.pr (Evd.ustate sigma) ++ fnl () ++
   v 1 (str "Normalized constraints:" ++ cut() ++
-       Univ.ContextSet.pr (Termops.pr_evd_level sigma) ctx)
+       PConstraints.ContextSet.pr (Termops.pr_evd_qvar sigma) (Termops.pr_evd_level sigma) ctx)
 
 (* Simulate the Intro(s) tactic *)
 let show_intro ~proof all =
@@ -553,13 +553,14 @@ let mk_sources () =
   let edges =
     let libs = Library.loaded_libraries () in
     List.fold_left (fun edges dp ->
-        let _, (_, csts) = Safe_typing.univs_of_library @@ Library.library_compiled dp in
+        let _, (_,(_,univ_csts)) =
+          Safe_typing.univs_of_library @@ Library.library_compiled dp in
         UnivConstraints.fold (fun cst edges -> add_edge cst (Library dp) edges)
-          csts edges)
+          univ_csts edges)
       edges libs
   in
   let edges =
-    List.fold_left (fun edges (ref,csts) ->
+    List.fold_left (fun edges (ref,(_,csts)) ->
         UnivConstraints.fold (fun cst edges -> add_edge cst (GlobRef ref) edges)
           csts edges)
       edges srcs
@@ -1422,7 +1423,7 @@ let vernac_sort ~poly l =
 let vernac_constraint ~poly l =
   if poly && not (Lib.sections_are_opened ()) then
     user_err
-                 (str"Polymorphic universe constraints can only be declared"
+                 (str"Polymorphic constraints can only be declared"
                   ++ str " inside sections, use Monomorphic Constraint instead.");
   DeclareUniv.do_constraint ~poly l
 
@@ -2129,7 +2130,7 @@ let check_may_eval env sigma redexp rc =
     else
       let env = Evarutil.nf_env_evar sigma env in
       let env = Environ.push_qualities qs env in
-      let env = Environ.push_context_set (us,csts) env in
+      let env = Environ.push_context_set QGraph.Static (us,csts) env in
       let c = EConstr.to_constr sigma c in
       let env = Safe_typing.push_private_constants env (Evd.seff_private @@ Evd.eval_side_effects sigma) in
       (* OK to call kernel which does not support evars *)
@@ -2173,7 +2174,7 @@ let vernac_global_check c =
   let sigma = Evd.collapse_sort_variables sigma in
   let senv = Global.safe_env() in
   let uctx = Evd.universe_context_set sigma in
-  let senv = Safe_typing.push_context_set ~strict:false uctx senv in
+  let senv = Safe_typing.push_context_set ~strict:false QGraph.Static uctx senv in
   let c = EConstr.to_constr sigma c in
   let j = Safe_typing.typing senv c in
   Prettyp.print_safe_judgment j ++