05f1cd9ee9ad90564ca921f5959251a200eb19a8
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2025-12-12 17:13:52 +0100

    Fixed comments

--- engine/polyFlags.mli
+++ engine/polyFlags.mli
@@ -1,16 +1,18 @@
 type t
 (** Set of flags for universe polymorphism, implicit sort polymorphism and cumulativity.
-   Note that implicit sort polymorphism and cumulativity only make sense for constructions
-   that are already polymorphic. This invariant is ensured by the smart constructor below.
+    Note that implicit sort polymorphism (not collapsing sort variables to Type) and
+    cumulativity only make sense for constructions that are already polymorphic.
+    This invariant is ensured by the smart constructor below.
  *)
 
-(** Raises a user error if [collapse_sort_variables] or [cumulative] is true but [univ_poly] isn't *)
+(** Raises a user error if [univ_poly = false] and either [collapse_sort_variables = false] or [cumulative = true]. *)
 val make : univ_poly:bool -> collapse_sort_variables:bool -> cumulative:bool -> t
 
-(** All [false] *)
+(** The [default] is monomorphic:
+    [univ_poly] and [cumulative] are [false], [collapse_sort_variables] is [true] *)
 val default : t
 
-(** Only sets the universe [level_polymorphism] flag.
+(** Only sets the universe [univ_poly] flag.
     Use with care, this probably indicates that the code does not handle
     [cumulative] constructions when it should. Code relying on elaboration should
     also support the [collapse_sort_variables] flag. *)
