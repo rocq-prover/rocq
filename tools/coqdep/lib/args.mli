74b17f60e3a60e10e1720b2ca0c8975d9b254e79
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2026-01-28 15:41:43 +0100

    Add -package option to coqdep.

--- tools/coqdep/lib/args.mli
+++ tools/coqdep/lib/args.mli
@@ -16,6 +16,7 @@ type t =
   ; noglob : bool
   ; ml_path : string list
   ; vo_path : (bool * string * string) list
+  ; packages : string list
   ; dyndep : string
   ; worker : string option
   ; files : string list
