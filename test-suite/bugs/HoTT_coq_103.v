79bd75bd24d03eaf5ebd0b0d2f04f0c2a6bc11a2
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2026-02-02 11:29:49 +0100

    Reverted HoTT_coq_103.v

--- test-suite/bugs/HoTT_coq_103.v
+++ test-suite/bugs/HoTT_coq_103.v
@@ -1,4 +1,4 @@
-Fail Check (nat : Type@{_}) : Set.
+Fail Check (nat : Type) : Set.
 (* Error:
 The term "nat:Type" has type "Type" while it is expected to have type
 "Set" (Universe inconsistency). *)
