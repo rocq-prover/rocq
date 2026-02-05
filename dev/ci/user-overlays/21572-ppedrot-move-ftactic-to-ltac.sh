676c3215f6910a27541a73d1aeba221cc8e58bf2
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2026-02-05 13:10:11 +0100

    Add overlays.

---
+++ dev/ci/user-overlays/21572-ppedrot-move-ftactic-to-ltac.sh
@@ -0,0 +1,5 @@
+overlay elpi https://github.com/ppedrot/coq-elpi move-ftactic-to-ltac 21572
+
+overlay equations https://github.com/ppedrot/Coq-Equations move-ftactic-to-ltac 21572
+
+overlay tactician https://github.com/ppedrot/coq-tactician move-ftactic-to-ltac 21572
