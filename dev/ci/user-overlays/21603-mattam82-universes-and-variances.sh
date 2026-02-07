5691ca12a59ea093e94d9cdef01ec85bffaaac61
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2026-02-07 15:02:33 +0100

    Add overlays

---
+++ dev/ci/user-overlays/21603-mattam82-universes-and-variances.sh
@@ -0,0 +1,5 @@
+overlay stdlib https://github.com/mattam82/stdlib universes-and-variances 21603
+overlay equations https://github.com/mattam82/Coq-Equations universes-and-variances 21603
+overlay elpi https://github.com/mattam82/coq-elpi universes-and-variances 21603
+overlay hb https://github.com/mattam82/hierarchy-builder universes-and-variances 21603
+overlay hott https://github.com/mattam82/hott universes-and-variances 21603
