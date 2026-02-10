ca1be7afe5f19a0c7906c27cff6b23250bbe6bd1
Pierre Roux <pierre.roux@onera.fr>
2026-02-10 09:06:05 +0100

    Add "if <term> is <pattern> then _ else _" syntax

---
+++ dev/ci/user-overlays/21609-proux01-if-is.sh
@@ -0,0 +1,3 @@
+overlay bedrock2 https://github.com/proux01/bedrock2 rocq21478 21609
+overlay itree https://github.com/proux01/InteractionTrees rocq21478 21609
+overlay unimath https://github.com/proux01/UniMath rocq21478 21609
