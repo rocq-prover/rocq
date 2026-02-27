86cd2d3b29e430e26ddab1eaefbd0c8559fbe418
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2026-02-27 13:54:51 +0100

    Stop inlining abstracted subproofs in tactics in terms.
    Fixes #7905: abstract in tactics in terms should not inline.

--- proofs/subproof.ml
+++ proofs/subproof.ml
@@ -16,7 +16,7 @@ module NamedDecl = Context.Named.Declaration
 (**********************************************************************)
 (* Shortcut to build a term using tactics *)
 
-let refine_by_tactic ~name ~poly env sigma ty tac =
+let refine_by_tactic ~name ~poly ?(inline = false) env sigma ty tac =
   (* Save the initial side-effects to restore them afterwards. *)
   let eff = Evd.eval_side_effects sigma in
   let old_len = Safe_typing.length_private @@ Evd.seff_private eff in
@@ -39,25 +39,31 @@ let refine_by_tactic ~name ~poly env sigma ty tac =
   | _ -> assert false
   in
   let ans = EConstr.to_constr ~abort_on_undefined_evars:false sigma ans in
-  (* [neff] contains the freshly generated side-effects *)
-  let neff = Evd.seff_private @@ Evd.eval_side_effects sigma in
-  let new_len = Safe_typing.length_private neff in
-  let neff, _ = Safe_typing.pop_private neff (new_len - old_len) in
-  (* Reset the old side-effects *)
-  let sigma = Evd.set_side_effects eff sigma in
+  let sigma, ans =
+    if inline then
+      (* [neff] contains the freshly generated side-effects *)
+      let neff = Evd.seff_private @@ Evd.eval_side_effects sigma in
+      let new_len = Safe_typing.length_private neff in
+      let neff, _ = Safe_typing.pop_private neff (new_len - old_len) in
+      (* Get rid of the fresh side-effects by internalizing them in the term
+        itself. Note that this is unsound, because the tactic may have solved
+        other goals that were already present during its invocation, so that
+        those goals rely on effects that are not present anymore. Hopefully,
+        this hack will work in most cases. *)
+      let (ans, uctx) = Safe_typing.inline_private_constants env ((ans, Univ.ContextSet.empty), neff) in
+      (* Reset the old side-effects *)
+      let sigma = Evd.set_side_effects eff sigma in
+      let sigma = Evd.merge_universe_context_set ~sideff:true UState.UnivRigid sigma uctx in
+      sigma, ans
+    else
+      sigma, ans
+  in
   (* Restore former goals *)
   let _goals, sigma = Evd.pop_future_goals sigma in
   (* Push remaining goals as future_goals which is the only way we
      have to inform the caller that there are goals to collect while
      not being encapsulated in the monad *)
   let sigma = List.fold_right Evd.declare_future_goal goals sigma in
-  (* Get rid of the fresh side-effects by internalizing them in the term
-     itself. Note that this is unsound, because the tactic may have solved
-     other goals that were already present during its invocation, so that
-     those goals rely on effects that are not present anymore. Hopefully,
-     this hack will work in most cases. *)
-  let (ans, uctx) = Safe_typing.inline_private_constants env ((ans, Univ.ContextSet.empty), neff) in
-  let sigma = Evd.merge_universe_context_set ~sideff:true UState.UnivRigid sigma uctx in
   EConstr.of_constr ans, sigma
 
 (* Abstract internals *)
