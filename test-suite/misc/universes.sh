06c1d84e6e35beee0ba848ed3740a3012d616ea5
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2026-03-10 18:12:08 +0100

    Down to 1 monomorphic universe above Set in the core library.
    Type.1 was previously generated by the (monomorphic) ssr_congr_arrow definition
    which talks about equality of types.

--- test-suite/misc/universes.sh
+++ test-suite/misc/universes.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Sort universes for the whole core library
-EXPECTED_UNIVERSES=2 # Prop is not counted
+EXPECTED_UNIVERSES=1 # Prop is not counted.
+# If ssr is univ poly we don't see the high universes involved in ssr_congr_arrow for example.
 $coqc -R misc/universes Universes misc/universes/all_stdlib 2>&1
 $coqc -R misc/universes Universes misc/universes/universes 2>&1
 mv universes.txt misc/universes
