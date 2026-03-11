b13f52e689bf210f4cd6d6dceb08b949880eb8a4
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-03-11 14:49:59 +0100

    metarocq overlay

--- dev/ci/user-overlays/21729-SkySkimmer-bignat.sh
+++ dev/ci/user-overlays/21729-SkySkimmer-bignat.sh
@@ -1,3 +1,5 @@
 overlay elpi https://github.com/SkySkimmer/coq-elpi bignat 21729
 
 overlay equations https://github.com/SkySkimmer/Coq-Equations bignat 21729
+
+overlay metarocq https://github.com/SkySkimmer/metarocq bignat 21729
