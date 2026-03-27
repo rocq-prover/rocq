260c6eb68f740f3f6741582339c3465a4b5f261d
Thomas Lamiaux <thomas.lamiaux@gmail.com>
2026-03-27 16:26:08 +0100

    try

--- tactics/induction.ml
+++ tactics/induction.ml
@@ -200,8 +200,8 @@ type eliminator =
 
 let is_nonrec env mind = (Environ.lookup_mind (fst mind) env).mind_finite == Declarations.BiFinite
 
-let find_ind_eliminator env sigma ?(dep=false) ind s =
-  let c = Elimschemes.lookup_eliminator env ~dep ind s in
+let find_ind_eliminator env sigma ind dep s =
+  let c = Elimschemes.lookup_eliminator env ind ~dep s in
   let sigma, c = EConstr.fresh_global env sigma c in
   sigma, destConst sigma c
 
@@ -1111,8 +1111,13 @@ let apply_induction_in_context with_evars inhyps elim indvars names =
       sigma, false, tac, indsign
     | ElimOver (id, (mind, u)) ->
        let dep_in_tmpcl = occur_var env sigma id tmpcl in
-       let dep = dep_in_hyps || dep_in_tmpcl in
-       let sigma, ind = find_ind_eliminator env sigma ~dep:dep mind s in
+       let dep =
+          (* true *)
+          (* dep_in_hyps ||*)
+          dep_in_tmpcl
+          (* || default_case_analysis_dependence env mind *)
+       in
+       let sigma, ind = find_ind_eliminator env sigma mind (Some dep) s in
        let elimt = Retyping.get_type_of env sigma (mkConstU ind) in
        let scheme = compute_elim_sig sigma elimt in
        let indsign = compute_scheme_signature sigma scheme id (mkIndU (mind, u)) in
