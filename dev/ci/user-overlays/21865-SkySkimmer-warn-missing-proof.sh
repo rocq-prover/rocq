0b00ab3d47e8e6086095c77442dd450f7dec2d9c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-04-02 18:40:00 +0200

    fix overlay

--- dev/ci/user-overlays/21865-SkySkimmer-warn-missing-proof.sh
+++ dev/ci/user-overlays/21865-SkySkimmer-warn-missing-proof.sh
@@ -9,6 +9,6 @@ overlay ext_lib https://github.com/SkySkimmer/coq-ext-lib warn-missing-proof 218
 
 overlay fcsl_pcm https://github.com/SkySkimmer/fcsl-pcm warn-missing-proof 21865
 
-overlay menhir https://gitlab.inria.fr/ggilbert/menhir badproof 21865
+overlay menhirlib https://gitlab.inria.fr/ggilbert/menhir badproof 21865
 
 overlay coquelicot https://gitlab.inria.fr/ggilbert/coquelicot badproof 21865
