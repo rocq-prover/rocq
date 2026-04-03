726aa9ce5b18811a3c95b0a7e8d5f017ac157b83
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-04-03 14:44:16 +0200

    Validate Proof: detect missing constraints, error if any issues found

--- test-suite/success/ValidateProof.v
+++ test-suite/success/ValidateProof.v
@@ -20,3 +20,24 @@ Proof.
   Fail Validate Proof.
 
 Abort.
+
+Goal Type.
+Proof.
+  exact_no_check Type.
+  Fail Validate Proof.
+Abort.
+
+Polymorphic Record Box@{s;} (A:Prop) : Type@{s;Set} := box { unbox : A }.
+Arguments box {_}. Arguments unbox {_}.
+
+From Ltac2 Require Import Ltac2 Constr.
+Import Constr.Unsafe.
+
+Polymorphic Lemma foo@{s;} (A:Prop) (x:Box@{s;} A) : A.
+Proof.
+  let ind := match reference:(Box) with Std.IndRef ind => ind | _ => Control.throw Assertion_failure end in
+  let case := Constr.Unsafe.case ind in
+  let c := make (Case case ('(fun _:Box@{s;} A => A), Relevance.relevant) NoInvert '&x [|'(fun (b:A) => b)|]) in
+  exact $c.
+  Fail Validate Proof.
+Abort.
