0b934b6b3e6279741f0c5b110160e84c6606a9a4
Enrico Tassi <Enrico.Tassi@Inria.fr>
2026-04-16 20:45:16 +0200

    ci: use Elpi 3.7.0

---
+++ dev/ci/user-overlays/21924-gares-bump-elpi-3.7.sh
@@ -0,0 +1,4 @@
+overlay elpi https://github.com/LPCIC/coq-elpi elpi-3.7 21924
+
+# unneeded and makes HB slower
+# overlay hierarchy_builder https://github.com/math-comp/hierarchy-builder hierarchy-builder.elpi-3.7 21924
