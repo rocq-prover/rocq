ba476414d4636b533fbb831c5f1270599effac76
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-04-24 13:54:20 +0200

    Remove compat coq-core libraries (not executables)
    Keep coq-core.kernel because it's used by dune coq mode.

---
+++ dev/ci/user-overlays/21955-SkySkimmer-rm-core.sh
@@ -0,0 +1,17 @@
+overlay coq_performance_tests https://github.com/SkySkimmer/coq-performance-tests rm-core 21955
+
+overlay simple_io https://github.com/SkySkimmer/coq-simple-io rm-core 21955
+
+overlay autosubst_ocaml https://github.com/SkySkimmer/autosubst-ocaml rm-core 21955
+
+overlay coqhammer https://github.com/SkySkimmer/coqhammer rm-core 21955
+
+overlay metarocq https://github.com/SkySkimmer/metarocq rm-core 21955
+
+overlay mtac2 https://github.com/SkySkimmer/Mtac2 rm-core 21955
+
+overlay smtcoq https://github.com/SkySkimmer/smtcoq rm-core 21955
+
+overlay tactician https://github.com/SkySkimmer/coq-tactician rm-core 21955
+
+overlay quickchick https://github.com/SkySkimmer/QuickChick rm-core 21955
