d1df313f3a4999c5568749ac5bdcc0b360f0dccc
Matthieu Sozeau <mattam@mattam.org>
2026-05-20 18:10:22 +0200

    Add overlay for math-comp

--- dev/ci/user-overlays/21603-mattam82-universes-and-variances.sh
+++ dev/ci/user-overlays/21603-mattam82-universes-and-variances.sh
@@ -2,4 +2,5 @@ overlay stdlib https://github.com/mattam82/stdlib universes-and-variances 21603
 overlay equations https://github.com/mattam82/Coq-Equations universes-and-variances 21603
 overlay elpi https://github.com/mattam82/coq-elpi universes-and-variances 21603
 overlay hierarchy_builder https://github.com/mattam82/hierarchy-builder universes-and-variances 21603
+overlay mathcomp https://github.com/mattam82/math-comp universes-and-variances 21603
 overlay hott https://github.com/mattam82/hott universes-and-variances 21603
