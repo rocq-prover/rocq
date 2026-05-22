bc119355391977f8c42bf45945b9ead01650cbf5
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2026-05-22 17:06:10 +0200

    Add new output tests

--- test-suite/output/cbn_alias_iota.v
+++ test-suite/output/cbn_alias_iota.v
@@ -7,9 +7,14 @@
 
 Set Printing Width 200.
 
+Definition id_nat (x : nat) := x.
 Definition fst_nat (x y : nat) := x.
 Definition snd_nat (x y : nat) := y.
 Definition third_nat (x y z : nat) := z.
+Definition pick3 (a b c d : nat) := c.
+Definition fst4 (a b c d : nat) := a.
+Definition pick4 (a b c d : nat) := d.
+Definition wrap2 (x y : nat) := y.
 
 Eval cbn in
   (fun y : nat =>
@@ -44,3 +49,64 @@ Eval cbn in
            | S _ => O
            end))
        O).
+
+Eval cbn in
+  (fun y : nat =>
+     id_nat
+       (match true with
+        | true => match y with O => S O | S _ => O end
+        | false => O
+        end)).
+
+Eval cbn in
+  (fun y : nat =>
+     pick3
+       O
+       (S O)
+       (match true with
+        | true => match y with O => S O | S _ => O end
+        | false => O
+        end)
+       (S (S O))).
+
+Eval cbn in
+  (fun y : nat =>
+     id_nat (id_nat
+       (match true with
+        | true => match y with O => S O | S _ => O end
+        | false => O
+        end))).
+
+Eval cbn in
+  (fun y : nat =>
+     fst4
+       (match true with
+        | true => match y with O => S O | S _ => O end
+        | false => O
+        end)
+       O O O).
+
+Eval cbn in
+  (fun y : nat =>
+     pick4 O O O
+       (match true with
+        | true => match y with O => S O | S _ => O end
+        | false => O
+        end)).
+
+Eval cbn in
+  (fun y : nat =>
+     S (id_nat
+          (match true with
+           | true => match y with O => S O | S _ => O end
+           | false => O
+           end))).
+
+Eval cbn in
+  (fun y : nat =>
+     wrap2
+       (if true then O else S O)
+       (match true with
+        | true => match y with O => S O | S _ => O end
+        | false => O
+        end)).
