66be6643b71a6478ca88ac69101a9094cbf5a132
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2026-05-28 13:11:09 +0200

    Add overlays.

---
+++ dev/ci/user-overlays/22070-ppedrot-kernel-abstract-univ-inductive.sh
@@ -0,0 +1,5 @@
+overlay elpi https://github.com/ppedrot/coq-elpi kernel-abstract-univ-inductive 22070
+
+overlay equations https://github.com/ppedrot/equations kernel-abstract-univ-inductive 22070
+
+overlay paramcoq https://github.com/ppedrot/paramcoq kernel-abstract-univ-inductive 22070
