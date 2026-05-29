33534059ca8981fbb8166ea49b69f4eb09a5a7de
Pierre Roux <pierre.roux@onera.fr>
2026-05-29 10:04:44 +0200

    [CI] Fix micromega dune package name

--- dev/ci/scripts/ci-micromega.sh
+++ dev/ci/scripts/ci-micromega.sh
@@ -10,6 +10,6 @@ git_download micromega
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 ( cd "${CI_BUILD_DIR}/micromega"
-  dune build --root . --only-packages=micromega-plugin @install
-  dune install --root . micromega-plugin --prefix=$CI_INSTALL_DIR
+  dune build --root . --only-packages=rocq-micromega-plugin @install
+  dune install --root . rocq-micromega-plugin --prefix=$CI_INSTALL_DIR
 )
