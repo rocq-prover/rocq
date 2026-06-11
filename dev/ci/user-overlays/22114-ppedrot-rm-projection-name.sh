20e3b31eb0572e190757f7d95baa48644c71a592
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2026-06-11 12:01:28 +0200

    Add overlays.

---
+++ dev/ci/user-overlays/22114-ppedrot-rm-projection-name.sh
@@ -0,0 +1,9 @@
+overlay coqhammer https://github.com/ppedrot/coqhammer rm-projection-name 22114
+
+overlay elpi https://github.com/ppedrot/coq-elpi rm-projection-name 22114
+
+overlay mtac2 https://github.com/ppedrot/Mtac2 rm-projection-name 22114
+
+overlay tactician https://github.com/ppedrot/coq-tactician rm-projection-name 22114
+
+overlay waterproof https://github.com/ppedrot/coq-waterproof rm-projection-name 22114
