90c018425ea890c3730c5c0fa09fc003de216c26
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2026-06-29 17:15:51 +0200

    Add support for `autorewrite <- with` (closes #1875)

--- plugins/ltac2/tac2stdlib.ml
+++ plugins/ltac2/tac2stdlib.ml
@@ -695,7 +695,7 @@ let () =
 let () =
   define "tac_autorewrite"
     (bool @-> option (thunk unit) @-> list ident @-> clause @-> tac unit)
-    (fun all by ids cl -> Tac2tactics.autorewrite ~all by ids cl)
+    (fun all by ids cl -> Tac2tactics.autorewrite ~all ~forward:true by ids cl)
 
 let () =
   define "tac_subst" (list ident @-> tac unit) Equality.subst
