coq/coq

Overlays for elpi, coq-lsp, autosubst-ocaml, quickchick and tactician

dev/ci/user-overlays/19404-herbelin-master+general-recursive-notations.sh
@@ -1,0 +1,5 @@
+overlay elpi https://github.com/herbelin/coq-elpi coq-master+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay coq_lsp https://github.com/herbelin/coq-lsp main+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay autosubst_ocaml https://github.com/herbelin/autosubst-ocaml master+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay quickchick https://github.com/herbelin/QuickChick master+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay tactician https://github.com/herbelin/coq-tactician coqdev+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations

rocq-prover/rocq

dev/ci/user-overlays/19404-herbelin-master+general-recursive-notations.sh
@@ -1,0 +1,6 @@
+overlay elpi https://github.com/herbelin/coq-elpi coq-master+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay rocq_lsp https://github.com/herbelin/coq-lsp main+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay autosubst_ocaml https://github.com/herbelin/autosubst-ocaml master+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay quickchick https://github.com/herbelin/QuickChick master+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay tactician https://github.com/herbelin/coq-tactician coqdev+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations
+overlay equations https://github.com/herbelin/coq-equations main+adapt-coq-pr19404-general-recursive-notations 19404 master+general-recursive-notations