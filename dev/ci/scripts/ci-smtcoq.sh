511c432a7d955f3d0e5386597cee8329e11b49a6
Chantal Keller <chantal.keller@universite-paris-saclay.fr>
2026-07-09 13:13:06 +0200

    Fix build script

--- dev/ci/scripts/ci-smtcoq.sh
+++ dev/ci/scripts/ci-smtcoq.sh
@@ -10,6 +10,6 @@ git_download smtcoq
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
 
 ( cd "${CI_BUILD_DIR}/smtcoq"
-  dune build --root . -only-packages=rocq-smtcoq @install
-  dune install -root . rocq-smtcoq --prefix="$CI_INSTALL_DIR"
+  dune build --root . --only-packages=rocq-smtcoq @install
+  dune install --root . rocq-smtcoq --prefix="$CI_INSTALL_DIR"
 )
