a374eadfaa54ffcc7611bb661df74549f7d0f37a
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2026-08-28 15:54:16 +0200

    Honor all registered modes when one mode is `=`
    (cherry picked from commit 3da8a136deb9d01f3984ba66f449e578ace1b515)

--- tactics/hints.mli
+++ tactics/hints.mli
@@ -135,6 +135,14 @@ module Hint_db :
         Returns a [ModeMismatch] if there are declared modes and none matches. *)
     val map_eauto : env -> evar_map -> secvars:Id.Pred.t -> (GlobRef.t * constr array) -> constr -> t -> FullHint.t list with_mode
 
+    (** As [map_eauto], but returns the nonempty list of distinct matching
+        mode restrictions in lookup order. The result is [None] when modes are
+        declared but none matches, and the mode list is [[NoMode]] when none
+        are declared. *)
+    val map_eauto_modes : env -> evar_map -> secvars:Id.Pred.t ->
+      (GlobRef.t * constr array) -> constr -> t ->
+      (mode_match list * FullHint.t list) option
+
     (** All hints associated to the reference.
         Precondition: no evars should appear in the arguments, so no modes
         are checked. *)
