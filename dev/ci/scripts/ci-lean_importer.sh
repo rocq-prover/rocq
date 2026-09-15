ce3c5ea86cdc8b1465894d004767c3b7324bfa75
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-09-15 14:08:20 +0200

    Force git mode for ci-lean_importer

--- dev/ci/scripts/ci-lean_importer.sh
+++ dev/ci/scripts/ci-lean_importer.sh
@@ -5,6 +5,7 @@ set -e
 ci_dir="$(dirname "$0")"
 . "${ci_dir}/ci-common.sh"
 
+WITH_SUBMODULES=1 # force git for LFS dumps used in tests
 git_download lean_importer
 
 if [ "$DOWNLOAD_ONLY" ]; then exit 0; fi
