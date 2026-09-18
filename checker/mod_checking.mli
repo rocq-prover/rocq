b48c8097594251dbe110eccb39b72709c0f98e59
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-09-18 15:37:19 +0200

    Experimental parallelism in rocqchk on opaques
    on HoTT we go from real: 15.58, user: 15.37, sys: 0.14, mem: 396968 ko
    to real: 10.79, user: 67.68, sys: 1.46, mem: 518756 ko

--- checker/mod_checking.mli
+++ checker/mod_checking.mli
@@ -18,3 +18,6 @@ exception BadConstant of Names.Constant.t * Pp.t
 
 val constants_of_opaques : Environ.env -> opaques -> Names.Constant.t list
 val empty_opaques : opaques
+
+val pool : Domainslib.Task.pool option ref
+val await : unit Domainslib.Task.promise list ref
